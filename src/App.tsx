69a2f4661ceed2c379f37e97c84e1ecd9b4bddc3
Jander Stival de Carvalho <janderscarvalho@gmail.com>
2023-10-16 12:08:35 -0300

    Criação do Header, Routes e Inicio de criação da Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import { Button } from './components/Button'
+import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
 
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
 
 export function App() {
- 
-
   return (
-    <>
-    <Button variant="primary"/>
-    <Button variant="secondary"/>
-    <Button variant="danger"/>
-    <Button variant="success"/>
-    </>
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
-
-
